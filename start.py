8d5c222ecf6f412182bd30d97374020effec8b68
Temitope Fagborun <flashestern09@gmail.com>
2023-12-13 04:25:53 +0100

    Initial Commit

---
+++ start.py
@@ -0,0 +1,14 @@
+"""Module providing a function printing python version."""
+# import sys
+
+# print(sys.version)
+# print(sys.executable)
+
+
+def greet(who_to_greet):
+    """Function to greet."""
+    return f'Hello, {who_to_greet}'
+
+
+print(greet("Temitope"))
+print(greet("Fagborun"))
