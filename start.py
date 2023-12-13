7ed02a98e64be8f7b89aa58e7bb4b3e37a74e1cd
Temitope Fagborun <flashestern09@gmail.com>
2023-12-13 04:37:58 +0100

    modified

--- start.py
+++ start.py
@@ -1,10 +1,3 @@
-"""Module providing a function printing python version."""
-# import sys
-
-# print(sys.version)
-# print(sys.executable)
-
-
 def greet(who_to_greet):
     """Function to greet."""
     return f'Hello, {who_to_greet}'
